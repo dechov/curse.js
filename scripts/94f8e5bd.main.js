88e745bd4b40f6ec518d5059538e83049307543b
Paul Dechov <dechovsky@gmail.com>
2013-06-22 08:49:59 +0000

    demo

---
+++ scripts/94f8e5bd.main.js
@@ -0,0 +1,1 @@
+$(function(){$(".curse-btn").animate({"padding-left":312},300).click(function(){return $(".text-container").curse({color:"hsl(136, 30%, 75%)",markPageNumbers:!0}),$(this).remove(),!1})});
